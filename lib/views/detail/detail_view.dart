7c703834bb0de64eead626d7929fbb3f063e863a
Erdal-Tanirkut <159000693+Erdal-Tanirkut@users.noreply.github.com>
2024-07-05 19:22:43 +0300

    detail page genel hatlarıyla tamamlandı(kendi yazdığım fake explore page ile beraber çalışıyor))
    youtubevideo player is initialized and used , provider is added to dependencies

--- lib/views/detail/detail_view.dart
+++ lib/views/detail/detail_view.dart
@@ -1,125 +1,185 @@
 import 'package:flutter/material.dart';
+import 'package:muse/views/detail/detail_widgets.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: ExploraPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class ExploraPage extends StatelessWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Explore'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            showModalBottomSheet(
+              context: context,
+              isScrollControlled: true,
+              backgroundColor: Colors.transparent,
+              builder: (BuildContext context) {
+                return DraggableScrollableSheet(
+                  expand: false,
+                  builder: (context, scrollController) {
+                    return Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(26.0),
+                          topRight: Radius.circular(26.0),
+                        ),
+                      ),
+                      child: SingleChildScrollView(
+                        controller: scrollController,
+                        child: ArtworkDetailPage(),
+                      ),
+                    );
+                  },
+                );
+              },
+            );
+          },
+          child: Text('Show Artwork Detail'),
+        ),
+      ),
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class ArtworkDetailPage extends StatefulWidget {
+  @override
+  _ArtworkDetailPageState createState() => _ArtworkDetailPageState();
+}
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+class _ArtworkDetailPageState extends State<ArtworkDetailPage> {
+  late YoutubePlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = YoutubePlayerController(
+      initialVideoId: 'd66ACsHlWbc', // YouTube video ID'sini buraya ekleyin
+      flags: YoutubePlayerFlags(
+        autoPlay: true,
+        mute: false,
+      ),
+    );
+
+    _controller.addListener(_videoListener);
+  }
+
+  void _videoListener() {
+    if (_controller.value.playerState == PlayerState.ended) {
+      _controller.seekTo(Duration.zero);
+      _controller.play();
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller.removeListener(_videoListener);
+    _controller.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    return Column(
+      children: [
+        Container(
+          height: 60,
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Positioned(
+                top: 10,
+                child: Container(
+                  width: 160,
+                  height: 40,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Center(
+                    child: Text(
+                      'Lorem Ipsum',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Center(child: CustomTextBox(label: "Digital Illustration", isTitle: false)),
+              SizedBox(height: 8.0),
+              Row(
+                children: [
+                  CustomTextBox(label: "Artist"),
+                  Spacer(),
+                  Text(
+                    "90€",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 24.0,
+                    ),
+                  ),
+                ],
+              ),
+              CustomTextBox(label: "Lorem Ipsum"),
+              CustomTextBox(label: "Size"),
+              CustomTextBox(label: "Lorem Ipsum"),
+              CustomTextBox(label: "Location"),
+              CustomTextBox(label: "Lorem Ipsum, Istanbul, Turkiye"),
+              SizedBox(height: 16.0),
+              CustomTextBox(
+                label:
+                "Lorem ipsum dolor sit amet consectetur. Dictum etiam duis nulla amet ultricies ultricies...",
+                isDescription: true,
+              ),
+              SizedBox(height: 45.0),
+              Center(
+                child: CustomButton(
+                  text: "Contact to Buy",
+                  onPressed: () {},
+                ),
+              ),
+              SizedBox(height: 16.0),
+              AspectRatio(
+                aspectRatio: 16 / 9,
+                child: YoutubePlayer(
+                  controller: _controller,
+                  showVideoProgressIndicator: true,
+                  progressIndicatorColor: Colors.red,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
+
+
