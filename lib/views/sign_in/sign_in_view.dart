1149e126ad5be9fe5d49a03c4aabafdbdee079a1
Elif Guven <elifserranur@gmail.com>
2024-07-06 18:40:12 +0300

    sign up ekranı ana hatlarıyla tamamlandı ve sign in ekranı guncellendi

--- lib/views/sign_in/sign_in_view.dart
+++ lib/views/sign_in/sign_in_view.dart
@@ -1,91 +1,131 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:muse/views/sign_up/sign_up_view.dart';
 
-class SignInView extends StatelessWidget {
+class SignInView extends StatefulWidget {
+  @override
+  _SignInViewState createState() => _SignInViewState();
+}
+
+class _SignInViewState extends State<SignInView> {
+  bool _isPasswordVisible = false; //to toggle visibility
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          //mainAxisAlignment: MainAxisAlignment.center, //left arrow da ekranın ortasına geliyor
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            IconButton(
-              alignment: Alignment.topLeft,
-              icon: Icon(Icons.arrow_back, color: Colors.red),
-              onPressed: () {
-                // TODO: Implement back navigation
-              },
-            ),
-            SizedBox(height: 50),
-            Text(
-              'Sign in now',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(height: 8),
-            Text(
-              'Please sign in to continue our app',
-              style: TextStyle(fontSize: 16, color: Colors.grey),
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(height: 32),
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Email',
-                hintText: 'example@gmail.com',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            SizedBox(height: 16),
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Password',
-                border: OutlineInputBorder(),
-                suffixIcon: Icon(Icons.visibility_off),
-              ),
-              obscureText: true,
-            ),
-            SizedBox(height: 16),
-            Align(
-              alignment: Alignment.centerRight,
-              child: TextButton(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              IconButton(
+                alignment: Alignment.topLeft,
+                icon: Icon(Icons.arrow_back, color: Colors.red),
                 onPressed: () {
-                  // TODO: Implement forgot password navigation
+                  // Navigate to onboarding
                 },
-                child: Text(
-                  'Forget Password?',
-                  style: TextStyle(color: Colors.red),
+              ),
+              SizedBox(height: 50),
+              Text(
+                'Sign in now',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 8),
+              Text(
+                'Please sign in to continue our app',
+                style: TextStyle(fontSize: 16, color: Colors.grey),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 32),
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  hintText: 'example@gmail.com',
+                  border: OutlineInputBorder(),
                 ),
               ),
-            ),
-            SizedBox(height: 32),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.red,
-                padding: EdgeInsets.symmetric(vertical: 16),
+              SizedBox(height: 16),
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  border: OutlineInputBorder(),
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      _isPasswordVisible
+                          ? Icons.visibility
+                          : Icons.visibility_off,
+                      color: Theme.of(context).primaryColorDark,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _isPasswordVisible = !_isPasswordVisible;
+                      });
+                    },
+                  ),
+                ),
+                obscureText: !_isPasswordVisible,
               ),
-              onPressed: () {
-                // TODO: Implement sign-in logic
-              },
-              child: Text(
-                'Sign In',
-                style: TextStyle(color: Colors.white, fontSize: 18),
-              ),
-            ),
-            SizedBox(height: 16),
-            Center(
-              child: TextButton(
-                onPressed: () {
-                  // TODO: Implement sign-up navigation
-                },
-                child: Text(
-                  "Don't have an account? Sign up",
-                  style: TextStyle(color: Colors.red),
+              SizedBox(height: 8),
+              Align(
+                alignment: Alignment.centerRight,
+                child: TextButton(
+                  onPressed: () {
+                    // Implement forgot password navigation
+                  },
+                  child: Text(
+                    'Forget Password?',
+                    style: TextStyle(color: Colors.red),
+                  ),
                 ),
               ),
-            ),
-          ],
+              SizedBox(height: 24),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.red,
+                  padding: EdgeInsets.symmetric(vertical: 16),
+                ),
+                onPressed: () {
+                  // Implement sign-in logic
+                },
+                child: Text(
+                  'Sign In',
+                  style: TextStyle(color: Colors.white, fontSize: 18),
+                ),
+              ),
+              SizedBox(height: 24),
+              Center(
+                child: RichText(
+                  text: TextSpan(
+                    style: const TextStyle(
+                      color: Colors.black54,
+                    ),
+                    children: <TextSpan>[
+                      const TextSpan(
+                        text: "Don't have an account? ",
+                      ),
+                      TextSpan(
+                        text: 'Sign up',
+                        style: const TextStyle(
+                          color: Colors.red,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => SignUpView()),
+                            );
+                          },
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
